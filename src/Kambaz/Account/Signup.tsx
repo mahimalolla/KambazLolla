993be3fc16b505b19b9629da07d3cd93f7f68290
Mahima Lolla <74255400+mahimalolla@users.noreply.github.com>
2025-06-03 19:13:23 -0400

    Update Signup.tsx

--- src/Kambaz/Account/Signup.tsx
+++ src/Kambaz/Account/Signup.tsx
@@ -19,6 +19,7 @@ export default function Signup() {
 
   return (
     <div style={{
+      marginLeft: '240px', // Account for navigation width
       minHeight: '100vh',
       display: 'flex',
       alignItems: 'center',
