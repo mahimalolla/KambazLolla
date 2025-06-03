b3ba4fed9fe1146f1bdc049f7530b03fdbedcca4
Mahima Lolla <74255400+mahimalolla@users.noreply.github.com>
2025-06-03 18:44:00 -0400

    Update index.tsx

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -1,24 +1,75 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import Account from "./Account";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
+import KambazNavigation from "./Navigation";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
-import KambazNavigation from "./Navigation";
-import Calendar from "./Calendar";
-import Inbox from "./Inbox";
+import Account from "./Account";
+import * as db from "./Database";
 
 export default function Kambaz() {
+  const [courses, setCourses] = useState<any[]>(db.courses);
+  const [course, setCourse] = useState<any>({
+    _id: "1234",
+    name: "New Course",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+    description: "New Description",
+    image: "/images/reactjs.jpg",
+  });
+
+  const addNewCourse = () => {
+    const newCourse = { 
+      ...course, 
+      _id: new Date().getTime().toString() 
+    };
+    setCourses([...courses, newCourse]);
+  };
+
+  const deleteCourse = (courseId: any) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+
   return (
     <div id="wd-kambaz">
       <KambazNavigation />
-      <Routes>
-        <Route path="/" element={<Navigate to="Account" />} />
-        <Route path="/Account/*" element={<div style={{marginLeft: '240px'}}><Account /></div>} />
-        <Route path="/Dashboard" element={<div style={{marginLeft: '240px'}}><Dashboard /></div>} />
-        <Route path="/Courses/:cid/*" element={<Courses />} />
-        <Route path="/Calendar" element={<Calendar />} />
-        <Route path="/Inbox" element={<Inbox />} />
-
-      </Routes>
+      <div className="wd-main-content-offset p-3">
+        <Routes>
+          <Route path="/" element={<Navigate to="Dashboard" />} />
+          <Route path="Account/*" element={<Account />} />
+          <Route
+            path="Dashboard"
+            element={
+              <Dashboard
+                courses={courses}
+                course={course}
+                setCourse={setCourse}
+                addNewCourse={addNewCourse}
+                deleteCourse={deleteCourse}
+                updateCourse={updateCourse}
+              />
+            }
+          />
+          <Route
+            path="Courses/:cid/*"
+            element={<Courses courses={courses} />}
+          />
+          <Route path="Calendar" element={<h1>Calendar</h1>} />
+          <Route path="Inbox" element={<h1>Inbox</h1>} />
+        </Routes>
+      </div>
     </div>
   );
 }
