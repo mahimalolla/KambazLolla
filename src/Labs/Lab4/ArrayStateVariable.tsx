ac7b7e70eb8efda49927ef76fff9d95576efc023
Mahima Lolla <74255400+mahimalolla@users.noreply.github.com>
2025-06-03 18:10:54 -0400

    Add files via upload

---
+++ src/Labs/Lab4/ArrayStateVariable.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+export default function ArrayStateVariable() {
+  const [array, setArray] = useState([1, 2, 3, 4, 5]);
+  
+  const addElement = () => {
+    setArray([...array, Math.floor(Math.random() * 100)]);
+  };
+  
+  const deleteElement = (index: number) => {
+    setArray(array.filter((_item, i) => i !== index));
+  };
+
+  return (
+    <div id="wd-array-state-variables">
+      <h2>Array State Variable</h2>
+      <button className="btn btn-success" onClick={addElement}>
+        Add Element
+      </button>
+      <ul className="list-group">
+        {array.map((item, index) => (
+          <li key={index} className="list-group-item">
+            {item}
+            <button
+              className="btn btn-danger float-end"
+              onClick={() => deleteElement(index)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+      <hr />
+    </div>
+  );
+}
