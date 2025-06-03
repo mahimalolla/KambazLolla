69e314e03527d9dfa274cbd11914d230899dadb1
Mahima Lolla <74255400+mahimalolla@users.noreply.github.com>
2025-06-02 22:02:06 -0400

    Update index.tsx

--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -1,15 +1,25 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, Link } from "react-router-dom";
 import Signin from "./Signin";
-import Signup from "./Signup";
 import Profile from "./Profile";
 
 export default function Account() {
   return (
     <div id="wd-account-screen">
+      <h2>Account</h2>
+      
+      {/* Add navigation links */}
+      <div style={{ marginBottom: '20px' }}>
+        <Link to="/Kambaz/Account/Signin" style={{ marginRight: '15px' }}>
+          Sign In
+        </Link>
+        <Link to="/Kambaz/Account/Profile">
+          Profile
+        </Link>
+      </div>
+      
       <Routes>
         <Route path="/" element={<Navigate to="/Kambaz/Account/Signin" />} />
         <Route path="/Signin" element={<Signin />} />
-        <Route path="/Signup" element={<Signup />} />
         <Route path="/Profile" element={<Profile />} />
       </Routes>
     </div>
