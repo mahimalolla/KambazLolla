1a23431e6caf552f9d716e9f1e5f22a62bea4928
Mahima Lolla <74255400+mahimalolla@users.noreply.github.com>
2025-06-03 21:33:15 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Labs from "./Labs";
 import Kambaz from "./Kambaz";
-import LandingPage from "./LandingPage";
-
 export default function App() {
   return (
     <HashRouter>
       <div>
         <Routes>
-          {/* Landing page with portfolio features */}
-          <Route path="/" element={<LandingPage />} />
-          
-          {/* Your existing routes */}
-          <Route path="/Labs/*" element={<Labs />} />
-          <Route path="/Kambaz/*" element={<Kambaz />} />
-          
-          {/* Portfolio features as separate routes (optional) */}
-          <Route path="/portfolio" element={<Navigate to="/#portfolio" />} />
-          <Route path="/blog" element={<Navigate to="/#blog" />} />
-          <Route path="/testimonials" element={<Navigate to="/#testimonials" />} />
+          <Route path="/" element={<Navigate to="Kambaz" />} />
+          <Route path="/Labs/" element={<Labs />} />
+          <Route path="/Kambaz/" element={<Kambaz />} />
         </Routes>
       </div>
     </HashRouter>
