6dfbfd30e2459c7fb95db6f3b9dd354b4d08bed9
Mahima Lolla <74255400+mahimalolla@users.noreply.github.com>
2025-06-15 01:32:05 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
+import { AuthProvider } from "./AuthContext"; 
 import Labs from "./Labs";
 import Kambaz from "./Kambaz";
 
 export default function App() {
   return (
-    <HashRouter>
-      <div>
-        <Routes>
-          {/* Redirect any unmatched routes to Kambaz */}
-          <Route path="*" element={<Navigate to="/Kambaz" />} />
-          <Route path="/Labs/*" element={<Labs />} />
-          <Route path="/Kambaz/*" element={<Kambaz />} />
-        </Routes>
-      </div>
-    </HashRouter>
+    <AuthProvider> {/* Wrap everything with AuthProvider */}
+      <HashRouter>
+        <div>
+          <Routes>
+            {/* Redirect any unmatched routes to Kambaz */}
+            <Route path="*" element={<Navigate to="/Kambaz" />} />
+            <Route path="/Labs/*" element={<Labs />} />
+            <Route path="/Kambaz/*" element={<Kambaz />} />
+          </Routes>
+        </div>
+      </HashRouter>
+    </AuthProvider>
   );
 }
