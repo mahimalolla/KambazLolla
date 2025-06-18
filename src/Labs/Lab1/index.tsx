305a108c13c1076c2364edc63308dae282ad2b87
Mahima Lolla <74255400+mahimalolla@users.noreply.github.com>
2025-06-17 23:16:39 -0400

    Update index.tsx

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -27,7 +27,8 @@ export default function Lab1() {
       {/* Ordered List */}
       <div id="wd-ol">
         <h4>Ordered List</h4>
-        <ol>
+        <h5>My Favorite Recipe</h5>
+        <ol id="wd-your-favorite-recipe">
           <li>Mix flour, sugar, and baking powder</li>
           <li>Add milk and eggs</li>
           <li>Stir until smooth</li>
@@ -46,11 +47,13 @@ export default function Lab1() {
       {/* Unordered List */}
       <div id="wd-ul">
         <h4>Unordered List</h4>
+        <h5>My Favorite Books</h5>
         <ul>
           <li>The Great Gatsby</li>
           <li>To Kill a Mockingbird</li>
           <li>1984</li>
         </ul>
+        <h5>Programming Languages</h5>
         <ul>
           <li>Python</li>
           <li>React</li>
@@ -85,15 +88,17 @@ export default function Lab1() {
       <div id="wd-images">
         <h4>Images</h4>
         <img
-          src="https://www.animalhospitalofclemmons.com/files/AdobeStock290844781.jpeg"
-          alt="Remote example"
-          width={150}
+          id="wd-starship"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+          alt="Starship"
+          width={400}
         />
         <br />
         <img
-          src="/images/local-image.jpg"
-          alt="Local example"
-          width={150}
+          id="wd-teslabot"
+          src="/images/teslabot.jpg"
+          alt="Tesla Bot"
+          height={200}
         />
       </div>
 
@@ -101,50 +106,93 @@ export default function Lab1() {
       <div id="wd-anchor">
         <h4>Links</h4>
         <a href="https://google.com" target="_blank" rel="noreferrer">Google</a><br />
-        <a href="https://github.com/mahimalolla" target="_blank" rel="noreferrer">My GitHub</a>
+        <a id="wd-github" href="https://github.com/mahimalolla" target="_blank" rel="noreferrer">My GitHub</a>
       </div>
 
       {/* Forms */}
       <div id="wd-forms">
         <h4>HTML Forms</h4>
         <form>
-          <label>Username</label>
-          <input type="text" name="username" className="form-control" /><br />
+          <h5>Text Fields</h5>
+          <label htmlFor="wd-text-fields-username">Username:</label>
+          <input id="wd-text-fields-username" type="text" placeholder="jdoe" /><br />
+          
+          <label htmlFor="wd-text-fields-password">Password:</label>
+          <input id="wd-text-fields-password" type="password" value="123@#$asd" /><br />
+          
+          <label htmlFor="wd-text-fields-first-name">First Name:</label>
+          <input id="wd-text-fields-first-name" type="text" title="John" /><br />
+          
+          <label htmlFor="wd-text-fields-last-name">Last Name:</label>
+          <input id="wd-text-fields-last-name" type="text" placeholder="Doe" value="Wonderland" title="The last name" /><br />
 
-          <label>Bio</label>
-          <textarea className="form-control" rows={3}></textarea><br />
+          <h5>Text Area</h5>
+          <label>Biography:</label><br />
+          <textarea id="wd-textarea" cols={30} rows={10}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </textarea><br />
 
-          <label>Gender</label><br />
-          <input type="radio" name="gender" value="male" /> Male<br />
-          <input type="radio" name="gender" value="female" /> Female<br />
+          <h5 id="wd-buttons">Buttons</h5>
+          <button type="button" onClick={() => alert("Life is Good!")} id="wd-all-good">
+            Hello World!
+          </button><br />
 
-          <label>Skills</label><br />
-          <input type="checkbox" /> HTML<br />
-          <input type="checkbox" /> CSS<br />
-          <input type="checkbox" /> JavaScript<br />
+          <h5>File Upload</h5>
+          <input type="file" /><br />
 
-          <label>Country</label><br />
-          <select>
-            <option>USA</option>
-            <option>India</option>
-            <option>Germany</option>
+          <h5 id="wd-radio-buttons">Radio Buttons</h5>
+          <label>Favorite movie genre:</label><br />
+          <input type="radio" name="radio-genre" id="wd-radio-comedy" />
+          <label htmlFor="wd-radio-comedy">Comedy</label><br />
+          <input type="radio" name="radio-genre" id="wd-radio-drama" />
+          <label htmlFor="wd-radio-drama">Drama</label><br />
+          <input type="radio" name="radio-genre" id="wd-radio-scifi" />
+          <label htmlFor="wd-radio-scifi">SciFi</label><br />
+          <input type="radio" name="radio-genre" id="wd-radio-fantasy" />
+          <label htmlFor="wd-radio-fantasy">Fantasy</label><br />
+
+          <h5 id="wd-checkboxes">Checkboxes</h5>
+          <label>Favorite movie genres:</label><br />
+          <input type="checkbox" name="check-genre" id="wd-chkbox-comedy" />
+          <label htmlFor="wd-chkbox-comedy">Comedy</label><br />
+          <input type="checkbox" name="check-genre" id="wd-chkbox-drama" />
+          <label htmlFor="wd-chkbox-drama">Drama</label><br />
+          <input type="checkbox" name="check-genre" id="wd-chkbox-scifi" />
+          <label htmlFor="wd-chkbox-scifi">SciFi</label><br />
+          <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy" />
+          <label htmlFor="wd-chkbox-fantasy">Fantasy</label><br />
+
+          <h4 id="wd-dropdowns">Dropdowns</h4>
+          <h5>Select One</h5>
+          <label htmlFor="wd-select-one-genre">Favorite movie genre:</label><br />
+          <select id="wd-select-one-genre">
+            <option value="COMEDY">Comedy</option>
+            <option value="DRAMA">Drama</option>
+            <option selected value="SCIFI">Science Fiction</option>
+            <option value="FANTASY">Fantasy</option>
           </select><br />
 
-          <label>Favorite Tools</label><br />
-          <select multiple>
-            <option>VSCode</option>
-            <option>WebStorm</option>
-            <option>Sublime</option>
+          <h5>Select Many</h5>
+          <label htmlFor="wd-select-many-genre">Favorite movie genres:</label><br />
+          <select multiple id="wd-select-many-genre">
+            <option value="COMEDY" selected>Comedy</option>
+            <option value="DRAMA">Drama</option>
+            <option value="SCIFI" selected>Science Fiction</option>
+            <option value="FANTASY">Fantasy</option>
           </select><br />
 
-          <label>Email</label>
-          <input type="email" /><br />
-          <label>Age</label>
-          <input type="number" /><br />
-          <label>Date</label>
-          <input type="date" /><br />
-          <label>Progress</label>
-          <input type="range" /><br />
+          <h4>Other HTML Field Types</h4>
+          <label htmlFor="wd-text-fields-email">Email:</label>
+          <input type="email" placeholder="jdoe@somewhere.com" id="wd-text-fields-email" /><br />
+          
+          <label htmlFor="wd-text-fields-salary-start">Starting Salary:</label>
+          <input type="number" value="100000" placeholder="1000" id="wd-text-fields-salary-start" /><br />
+          
+          <label htmlFor="wd-text-fields-rating">Rating:</label>
+          <input type="range" value="4" max="5" id="wd-text-fields-rating" /><br />
+          
+          <label htmlFor="wd-text-fields-dob">Date of Birth:</label>
+          <input type="date" value="2000-01-21" id="wd-text-fields-dob" /><br />
 
           <button type="submit">Submit</button>
         </form>
