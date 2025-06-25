8c38613f35a3bf8aa399d8a92c9d70977c411903
Mahima Lolla <74255400+mahimalolla@users.noreply.github.com>
2025-06-25 19:37:48 -0400

    Update client.ts

--- src/Kambaz/Courses/Quizzes/client.ts
+++ src/Kambaz/Courses/Quizzes/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER || "http://localhost:4000";
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER || "https://kambaz-node.onrender.com";
 const QUIZZES_API = `${REMOTE_SERVER}/api/courses`;
 
 // ================================
